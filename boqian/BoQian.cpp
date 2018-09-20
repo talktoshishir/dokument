7359c947010800445ff37f88be90b44ac8af3a48
talktoshishir <30610740+talktoshishir@users.noreply.github.com>
2018-09-20 06:56:01 +0530

    BoQian First Checking template

---
+++ boqian/BoQian.cpp
@@ -0,0 +1,18 @@
+//============================================================================
+// Name        : BoQian.cpp
+// Author      : Shishir Pandey
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+#include "template.hh"
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	test1();
+	return 0;
+}
